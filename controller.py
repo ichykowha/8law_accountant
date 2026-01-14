d7139e4b1e352b506d22ebcf80389b8ccf494a32
ichykowha <mattgrapko@gmail.com>
2026-01-13 18:24:12 -0800

    Update controller.py

--- controller.py
+++ controller.py
@@ -7,18 +7,16 @@ class PowerhouseAccountant:
         self.query_engine = DataQueryAssistant()
         self.librarian = DocumentLibrarian()
 
+    # The New Version accepts 'doc_type'
     def process_document(self, file_path, username="admin", doc_type="financial"):
         # 1. Get filename
         import os
         file_name = os.path.basename(file_path)
         
-        # 2. Hand to Librarian (With doc_type!)
+        # 2. Hand to Librarian
         status = self.librarian.upload_document(file_path, file_name, username, doc_type)
         return status
 
     def process_input(self, user_text, history=[]):
-        # 1. Search Memory (Now searches both Library + User Data)
         clues = self.librarian.search_memory(user_text)
-        
-        # 2. Ask Brain
         return self.query_engine.ask(user_text, history_context=history, document_clues=clues)

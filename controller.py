b5abc506fccaca9096de7df3e1e4cb4a3a97bc2e
ichykowha <mattgrapko@gmail.com>
2026-01-13 17:40:36 -0800

    Update controller.py

--- controller.py
+++ controller.py
@@ -7,18 +7,17 @@ class PowerhouseAccountant:
         self.query_engine = DataQueryAssistant()
         self.librarian = DocumentLibrarian()
 
-    # The fix is here: We added 'username="admin"' so it accepts the name
-    def process_document(self, file_path, username="admin"):
+    def process_document(self, file_path, username="admin", doc_type="financial"):
         # 1. Get filename
         import os
         file_name = os.path.basename(file_path)
         
-        # 2. Hand to Librarian (Pass the username along!)
-        status = self.librarian.upload_document(file_path, file_name, username)
+        # 2. Hand to Librarian (With doc_type!)
+        status = self.librarian.upload_document(file_path, file_name, username, doc_type)
         return status
 
     def process_input(self, user_text, history=[]):
-        # 1. Search Memory
+        # 1. Search Memory (Now searches both Library + User Data)
         clues = self.librarian.search_memory(user_text)
         
         # 2. Ask Brain
